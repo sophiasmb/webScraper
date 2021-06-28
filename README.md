fd7a2fe9270d0b5afccdc8414357daab714a9afb
Sophia Barnett <smbarnett1@buffs.wtamu.edu>
2021-06-28 13:44:00 -0500

    Updated link access to get 3rd link in article and scan article. Also added resources to readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # webScraper
 Basic web scraper for learning purposes
+
+## Resources
+News information: (https://www.nbcnews.com/us-news)
+Beautiful Soup: (https://beautiful-soup-4.readthedocs.io/en/latest/#quick-start)
+Scraping from links: (https://www.geeksforgeeks.org/beautifulsoup-scraping-link-from-html/)
+Regex: (https://www.w3schools.com/python/python_regex.asp#findall)
+Regex Generator: (https://regexr.com/)
--- webscraper.py
+++ webscraper.py
@@ -13,10 +13,30 @@ Revision History:
 # 3. Set up website to show the title and subscript of the articles
 # 4. Search multiple different national news websites
 
+from requests.api import request
 from bs4 import BeautifulSoup
 import requests
 import json
 import re
+
+matches = ["Texas", "Tx", "Dallas", "Houston", "Austin", "Midland", "Odessa", "Lubbock"]
+
+# function to scan the full article for the words searching for
+def scan_article(url):
+    res = requests.get(url, timeout=5)
+    content = BeautifulSoup(res.content, "html.parser")
+
+    try:
+        for paragraph in content.find("div", attrs={"class": "article-body__content"}):
+            if any(x in paragraph.text for x in matches):
+                return True
+            # end if
+        # end for
+    except:
+        return False
+
+    return False
+# end scan_article
 
 def main():
     # Make a get request to get information from website
@@ -26,25 +46,47 @@ def main():
 
     # Put raw info in file to further examination
     art = content.find("div", attrs={"class": "feeds__items-wrapper ph5 ph0-m"}).prettify()
+    #art = content.find("div", attrs={"class": "multi-up__articles bg-knockout-primary"}).prettify()
     with open('output.txt', 'w') as f:
         f.write(str(art))
 
-    # Create an array of objects for each headline article on the 'us news' page
     articles = []
+
+    # Get top three stories (They do not include subscript)
+    for topStory in content.find("div", attrs={"class": "multi-up__articles bg-knockout-primary"}):
+        topStoryObj = {
+            "category": topStory.find("span", attrs={"data-test": "unibrow-text"}).text,
+            "posted": "",
+            "title": topStory.find("span", attrs={"class": "tease-card__headline"}).text,
+            "subscript": "",
+            "link": ""
+        }
+        articles.append(topStoryObj)
+
+        if any(x in (topStoryObj["title"] or topStoryObj["subscript"]) for x in matches):
+            print(topStoryObj["title"])
+
+    # Create an array of objects for each headline article on the 'us news' page
     for article in content.find("div", attrs={"class": "feeds__items-wrapper ph5 ph0-m"}):
-        link =  article.find('a', attrs={'href': re.compile('https:..www.nbcnews.com.*.{9,}')})
+        links = []
+        for link in article.find_all('a', attrs={'href': re.compile("^https://")}):
+            links.append(link)
         articleObject = {
             "category": article.find("span", attrs={"data-test": "unibrow-text"}).text,
             "posted": article.find("div", attrs={"class": "wide-tease-item__timestamp dib db-m ml3 ml0-m"}).text,
             "title": article.find("h2", attrs={"class": "wide-tease-item__headline"}).text,
             "subscript": article.find("div", attrs={"class": "wide-tease-item__description"}).text,
-            "link": link.get('href')
+            "link": links[2].get('href')
         }
         articles.append(articleObject)
 
-        matches = ["Texas", "Tx", "Dallas", "Houston", "Austin", "Midland", "Odessa", "Lubbock"]
         if any(x in (articleObject["title"] or articleObject["subscript"]) for x in matches):
             print(articleObject["title"])
+        elif scan_article(articleObject['link']):
+            print(articleObject["title"])
+
+    #if scan_article(articles[5]['link']):
+    #    print(articles[5]["title"])
 
     # store data as a json file
     with open("articleData.json", "w") as outfile:
